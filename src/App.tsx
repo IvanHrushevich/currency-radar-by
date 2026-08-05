67222c5fc6e15811a876bf6b2c281171dbb63991
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2026-08-05 20:39:05 +0300

    feat: init empty react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { JSX } from 'react'
+import './App.css'
+
+function App(): JSX.Element {
+  return (
+    <main className="app">
+      <h1>Currency Radar BY</h1>
+    </main>
+  )
+}
+
+export default App
